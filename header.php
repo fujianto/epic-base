fc87358cf9ecd9eb1c454f18c10468d6def64daa
Fujianto <septianahmad@naisinpo.com>
2016-05-12 12:34:10 +0700

    Implement custom logo WP 4.5.1

--- header.php
+++ header.php
@@ -30,14 +30,24 @@
 				<div class="col-md-4" <?php hybrid_attr( 'branding' ); ?>>
 
 					<div class="branding-wrapper">
-						<?php if ( display_header_text() ) : // If user chooses to display header text. ?>
+						<?php if ( get_custom_logo() == "" || get_custom_logo() == null  ) : // If custom logo exist. ?>
 
-							<?php hybrid_site_title(); ?>
-							<?php hybrid_site_description(); ?>
+							<?php if ( display_header_text() ) : // If user chooses to display header text. ?>
+
+								<?php hybrid_site_title(); ?>
+								<?php hybrid_site_description(); ?>
+
+							<?php endif; // End check for header text. ?>
+						
+						<?php else: ?>
+							
+							<?php if ( function_exists( 'the_custom_logo' ) ): ?>
+								<?php echo the_custom_logo(); ?>
+							<?php endif; // End check for header text. ?>
 
 						<?php endif; // End check for header text. ?>
-					</div>
-				
+					</div>		
+
 				</div>
 				<!-- End branding -->
 				
